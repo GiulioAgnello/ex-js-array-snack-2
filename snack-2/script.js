73ce160d223b83e259bde4cb34180e524bd91e84
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-08-03 11:48:10 +0200

    end Snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,5 +1,21 @@
 import books from "../database.js";
 
+// trovo libri disponibili
 const avalableBooks = books.filter((b) => b.available === true);
 
 console.log(avalableBooks);
+
+// discunt price
+const discountedBooks = avalableBooks.map((b) => {
+  const priceNumber = parseFloat(b.price);
+  return {
+    ...b,
+    price: priceNumber * 0.2,
+  };
+});
+console.log(discountedBooks);
+
+// trovo solo prezzi interi
+const fullPricedBook = discountedBooks.find((b) => parseInt(b.price));
+
+console.log(fullPricedBook);
