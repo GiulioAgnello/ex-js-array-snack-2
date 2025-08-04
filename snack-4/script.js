50f86457c21f8d1a661654d9f6dcbbf4f25e36b0
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-08-04 10:14:00 +0200

    snack4end

--- snack-4/script.js
+++ snack-4/script.js
@@ -1,1 +1,20 @@
+import books from "../database.js";
 
+console.log(books);
+
+// Creare un array (ages) che contiene le età degli autori dei libri.
+// Calcola la somma delle età (agesSum) usando reduce.
+// Stampa in console l’età media degli autori dei libri.
+
+const ages = books.map((b) => {
+  const autors = b.author;
+  return autors.age;
+});
+
+console.log(ages);
+
+let initialValue = 0;
+
+const agesSum = ages.reduce((acc, curr) => acc + curr, initialValue);
+
+console.log(agesSum / ages.length);
