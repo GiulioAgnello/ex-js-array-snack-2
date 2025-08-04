1cb4709dc8b768711ea026e9f5e0b069e54e3eee
agnellogiulio4@gmail.com <agnellogiulio4@gmail.com>
2025-08-04 10:06:01 +0200

    sortRequest

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,3 +1,27 @@
 import books from "../database.js";
 
 console.log(books);
+
+// array con tutti i nomi degli autori
+
+const authors = books.map((b) => {
+  const author = b.author;
+  return author;
+});
+
+console.log(authors);
+
+// Crea una variabile booleana (areAuthorsAdults)
+// per verificare se gli autori sono tutti maggiorenni.
+
+const areAuthorsAdults = authors.every((a) => a.age >= 18);
+
+console.log(areAuthorsAdults);
+
+// Ordina l’array authors in base all’età, senza creare un nuovo array.
+// (se areAuthorsAdult è true, ordina in ordine crescente, altrimenti in ordine decrescente)
+authors.sort((a, b) => {
+  return areAuthorsAdults ? a.age - b.age : b.age - a.age;
+});
+
+console.log(authors);
